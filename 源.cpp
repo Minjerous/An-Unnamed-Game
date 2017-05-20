c50e2d77744973397a4e09502abbc21688e30853
Rhyther <2815478278qq@gmail.com>
2017-05-20 13:15:44 +0800

    v1.0.7

--- 源.cpp
+++ 源.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 string Name; 
+int anjian;
 int HP = 100, GongJi = 20, FangYu = 20;
 int Level = 1, Exp = 0; int MaxExp;
 int Money = 0;
@@ -27,8 +28,149 @@ void bag()
 }
 void map()
 {
-	int zero = 0;
-	cout << 0 / zero;
+	while (1 == 1)
+	{
+		cout << "1)银河系 2）河外星系(未开启) 3)返回\n";
+		cin >> anjian;
+		cout << "\n\n\n\n\n\n\n\n\n\n";
+		if (anjian == 1)									//银河系
+		{
+			while (1 == 1)
+			{
+				cout << "1)太阳系 2）其他星系 3)返回\n";
+				cin >> anjian;
+				cout << "\n\n\n\n\n\n\n\n\n\n";
+				if ( anjian == 1)							//太阳系
+				{
+					while (1 == 1)
+					{
+						cout << "1)地球 2)其他行星或恒星及陨石带等 3)返回\n";
+						cin >> anjian;
+						cout << "\n\n\n\n\n\n\n\n\n\n";
+						if (anjian == 1)
+						{
+							while (1 == 1)
+							{
+								cout << "1中国 2）外国 3）返回\n"; cin >> anjian;
+								cout << "\n\n\n\n\n\n\n\n\n\n";
+								if (anjian == 1)			//中国
+								{
+									while (1 == 1)
+									{
+										cout << "1)山东省 2)其他省份 3)返回\n";
+										cin >> anjian;
+										cout << "\n\n\n\n\n\n\n\n\n\n"; 
+										if (anjian == 1)
+										{
+											while (1 == 1)
+											{
+												cout << "1)滨州市 2)其他市 3)返回\n";
+												cin >> anjian;
+												cout << "\n\n\n\n\n\n\n\n\n\n";
+												if (anjian == 1)
+												{
+													while (1 == 1)
+													{
+														cout << "1)惠民县 2)其他县 3)返回\n";
+														cin >> anjian;
+														cout << "\n\n\n\n\n\n\n\n\n\n";
+														if (anjian == 1)
+														{
+															while (1 == 1)
+															{
+																cout << "1)惠民县晨光实验学校 2)其他地方 3)返回\n";
+																cin >> anjian;
+																cout << "\n\n\n\n\n\n\n\n\n\n";
+																if (anjian == 1)
+																{
+																	while (1 == 1)
+																	{
+																		int zero = 0;
+																		cout << 0 / zero;
+																	}
+																}
+																else if (anjian == 2)
+																{
+																	cout << "该关卡未开启\n";
+																}
+																else
+																{
+																	break;
+																}
+															}
+														}
+														else if (anjian == 2)
+														{
+															cout << "该关卡未开启\n";
+														}
+														else
+														{
+															break;
+														}
+													}
+												}
+												else if (anjian == 2)
+												{
+													cout << "该关卡未开启\n";
+												}
+												else
+												{
+													break;
+												}
+											}
+										}
+										else if (anjian == 2)
+										{
+											cout << "该关卡未开启\n";
+										}
+										else
+										{
+											break;
+										}
+									}
+								}
+								else if (anjian == 2)
+								{
+									cout << "该关卡未开启\n";
+								}
+								else
+								{
+									break;
+								}
+								
+							}
+						}
+						else if (anjian == 2)
+						{
+							cout << "该关卡未开启\n";
+						}
+						else
+						{
+							break;
+						}
+					}
+				}
+				else if (anjian == 2)
+				{
+					cout << "该关卡未开启\n";					//其他星系
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		else if (anjian == 2)								//河外星系
+		{
+			cout << "该关卡未开启\n";
+		}
+		else												//返回
+		{
+			break;
+		}
+	}
+
+	
 }
 void LevelUp(int GongJi_, int FangYu_, int HP_, int Level_, int Exp_)
 {
@@ -78,6 +220,14 @@ void CheckName()
 	{
 		cout << "新余，你的许愿树成长度又满了....\n\n";
 	}
+	else if (Name == "刘希涛" || Name == "希涛" || Name == "liuxitao" || Name == "LIUXITAO" || Name == "xitao" || Name == "XITAO" )
+	{
+		cout << "希涛，看见我物理配套来没？？？\n\n";
+	}
+	else if (Name == "孔龙" || Name == "恐龙" || Name == " konglong" || Name == "KONGLONG" )
+	{
+		cout << "这是一个彩蛋，不是恐龙蛋。。。\n\n";
+	}
 	else
 	{
 		cout << "你好，" << Name << "，欢迎进入这个逗比的游戏。" << endl << endl;
@@ -85,11 +235,10 @@ void CheckName()
 }
 void DaiDing()
 {
-	int anjian;
 	while (1==1)
 	{
 		cout << "请选择你要做什么：\n";
-		cout << "1)背包(开发中)\n2)地图(开发中)\n3)状态\n";
+		cout << "1)背包(开发中)\n2)地图\n3)状态\n";
 		cin >> anjian;
 		cout << "\n\n\n\n\n\n\n\n\n\n";
 		switch (anjian)
@@ -104,7 +253,7 @@ void DaiDing()
 
 int main()
 {
-	Version(1.06f);
+	Version(1.07f);
 	CheckName();
 	zhuangtai(GongJi, FangYu, HP, Money, Level, Exp);
 	system("pause"); 
