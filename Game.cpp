cada670f150c7b8f95d752ccb894159394ed53d5
Rhyther <2815478278qq@gmail.com>
2017-05-20 14:42:39 +0800

    v1.1.0

--- Game.cpp
+++ Game.cpp
@@ -1,17 +1,18 @@
 ﻿#include <iostream>
+#include <cstdlib> 
 #include <string>
 using namespace std;
 
 string Name; 
 int anjian;
-int HP = 100, GongJi = 20, FangYu = 20;
+int Hp = 50, GongJi = 10, FangYu = 0;
 int Level = 1, Exp = 0; int MaxExp;
 int Money = 0;
 
-void bag();
-void map();
-void about();
-void role(int GongJi_, int FangYu_, int HP_,int Money_,int Level_,int Exp_); 
+void Bag();
+void Map();
+void About();
+void Role(int GongJi_, int FangYu_, int HP_,int Money_,int Level_,int Exp_); 
 void LevelUp(int GongJi_, int FangYu_, int HP_, int Level_, int Exp_);
 void CheckName();
 
@@ -33,7 +34,7 @@ class Guanqia
 		void get()
 		{
 			cout<<number<<")\t"<<name<<"\t";
-			if(kaiqi == true)
+			if(kaiqi == true)//判断是否开启或占领并输出 
 			{
 				if(zhanling == true)
 				{
@@ -49,21 +50,34 @@ class Guanqia
 				cout<<"未开启\n";
 			}
 		}
-		void set(int _zhanling,int _kaiqi)
+		void set(int _zhanling,int _kaiqi)//更改是否开启或占领 
 		{
 			zhanling = _zhanling;
 			kaiqi = _kaiqi;
 		}
 };
 Guanqia a_a;Guanqia a_b;Guanqia a_c;Guanqia a_d;Guanqia a_e;Guanqia a_f;Guanqia a_g;Guanqia a_h;Guanqia a_i;Guanqia a_j;
+class boss
+{
+	public:
+		string name;
+		int Hp;
+		int GongJi;
+		int FangYu;
+		class skill
+		{
+			string name;
+			int hurt;
+			
+		};
+};
 int main()
 {
-	
 	CheckName();
 	system("pause"); 
 	system("cls"); 
 	cout<<"你现在的属性为：\n" ;
-	role(GongJi, FangYu, HP, Money, Level, Exp);
+	Role(GongJi, FangYu, Hp, Money, Level, Exp);
 	a_a.init(1,"大操场  ") ;a_a.set(0,1);
 	a_b.init(2,"大门    ") ;
 	a_c.init(3,"男生宿舍") ;
@@ -78,15 +92,15 @@ int main()
 	while (1 == 1)
 	{
 		cout << "请选择你要做什么：\n";
-		cout << "1)背包(开发中)\n2)地图\n3)角色\n4)关于\n";
+		cout << "1)背包\n2)地图\n3)角色\n4)关于\n";
 		cin >> anjian;
 		system("cls"); 
 		switch (anjian)
 		{
-			case 1:bag(); break;
-			case 2:map(); break;
-			case 3:role(GongJi, FangYu, HP, Money, Level, Exp);  break;
-			case 4:about();break; 
+			case 1:Bag(); break;
+			case 2:Map(); break;
+			case 3:Role(GongJi, FangYu, Hp, Money, Level, Exp);  break;
+			case 4:About();break; 
 			default:cout << "输入错误\n\n";
 		}
 	}
@@ -95,19 +109,19 @@ int main()
 
 
 
-void about()
+void About()
 {
 	cout<<"关于："<<endl; 
-	cout << "版本：1.09" << endl;
+	cout << "版本：1.10" << endl;
 	system("pause"); 
 	system("cls"); 
 }
-void role(int GongJi_, int FangYu_, int HP_,int Money_,int Level_,int Exp_)
+void Role(int GongJi_, int FangYu_, int HP_,int Money_,int Level_,int Exp_)
 {
-	int ZhanDouLi = GongJi * 2 + FangYu * 2 + HP;
+	int ZhanDouLi = GongJi * 2 + FangYu * 2 + Hp;
 	int MaxExp = Level * 10;
 	cout << "战斗力:" << ZhanDouLi << "；\n\n";
-	cout << "生命值:" << HP << "；\n\n";
+	cout << "生命值:" << Hp << "；\n\n";
 	cout << "攻击力:" << GongJi << "；\n";
 	cout << "防御力:" << FangYu << "；\n\n";
 	cout << "等级:" << Level << "级；\n";
@@ -117,12 +131,84 @@ void role(int GongJi_, int FangYu_, int HP_,int Money_,int Level_,int Exp_)
 	system("pause"); 
 	system("cls"); 
 }
-void bag()
+void Bag()
 {
-	int zero = 0;
-	cout << 0 / zero;
+	while(1 == 1)
+	{
+		cout<<"1)"<<endl;
+		cout<<"2)"<<endl;
+		cout<<"3)"<<endl;
+		cout<<"4)"<<endl;
+		cout<<"5)"<<endl;
+		cout<<"6)"<<endl;
+		cout<<"7)"<<endl;
+		cout<<"8)"<<endl;
+		cout<<"9)"<<endl;
+		cout<<"1~9）选择物品 0)返回"<<endl; 
+		cin>>anjian;
+		system("cls") ;
+		if(anjian == 1)
+		{
+			cout<<"该处没有物品"<<endl;
+			system("pause");
+			system("cls") ;
+		}
+		else if(anjian == 2)
+		{
+			cout<<"该处没有物品"<<endl;
+			system("pause");
+			system("cls") ;
+		}
+		else if(anjian == 3)
+		{
+			cout<<"该处没有物品"<<endl;
+			system("pause");
+			system("cls") ;
+		}
+		else if(anjian == 4)
+		{
+			cout<<"该处没有物品"<<endl;
+			system("pause");
+			system("cls") ;
+		}
+		else if(anjian == 5)
+		{
+			cout<<"该处没有物品"<<endl;
+			system("pause");
+			system("cls") ;
+		}
+		else if(anjian == 6)
+		{
+			cout<<"该处没有物品"<<endl;
+			system("pause");
+			system("cls") ;
+		}
+		else if(anjian == 7)
+		{
+			cout<<"该处没有物品"<<endl;
+			system("pause");
+			system("cls") ;
+		}
+		else if(anjian == 8)
+		{
+			cout<<"该处没有物品"<<endl;
+			system("pause");
+			system("cls") ;
+		}
+		else if(anjian == 9)
+		{
+			cout<<"该处没有物品"<<endl;
+			system("pause");
+			system("cls") ;
+		}
+		else if(anjian == 0)
+		{
+			break;
+		}
+		
+	}
 }
-void map()
+void Map()
 {
 	while (1 == 1)
 	{
@@ -147,7 +233,7 @@ void map()
 						{
 							while (1 == 1)
 							{
-								cout << "1中国 2）外国 3）返回\n"; cin >> anjian;
+								cout << "1)中国 2）外国 3）返回\n"; cin >> anjian;
 								system("cls");
 								if (anjian == 1)			//中国
 								{
@@ -191,7 +277,7 @@ void map()
 																		a_h.get();
 																		a_i.get();
 																		a_j.get();
-																		cout<<"11)\t返回\n";	
+																		cout<<"0)\t返回\n";	
 																		cin >> anjian;
 																		system("cls"); 
 																		if (anjian == 1)
@@ -204,9 +290,27 @@ void map()
 																		}
 																		else if (anjian == 2)
 																		{
-																			cout << "该关卡未开启\n";
+																			cout << "你已经到达了学校大门\n";
 																			system("pause"); 
 																			system("cls"); 
+																			cout << "你发现了一个看门的\n";
+																			system("pause"); 
+																			system("cls"); 
+																			cout << "是否迎战\n1)是 0)否\n";
+																			cin >> anjian ;
+																			system("cls"); 
+																			if(anjian == 1)
+																			{
+																				cout<<"正在努力开发战斗资源z中，稍安勿躁。。。\n";
+																				system("pause");
+																				system("cls") ;
+																			}
+																			else if(anjian == 0)
+																			{
+																				cout<<"你失败了\n";
+																				system("pause");
+																				system("cls");
+																			}
 																		}
 																		else if (anjian == 3)
 																		{
@@ -359,7 +463,7 @@ void map()
 
 	
 } 
-void LevelUp(int GongJi_, int FangYu_, int HP_, int Level_, int Exp_)
+void LevelUp(int GongJi_, int FangYu_, int Hp_, int Level_, int Exp_)
 {
 	int MaxExp_ = 10 * Level_;
 	while(Exp_ >= MaxExp_)
@@ -369,7 +473,7 @@ void LevelUp(int GongJi_, int FangYu_, int HP_, int Level_, int Exp_)
 		MaxExp_ = 10 * Level_;
 		GongJi_ += 4;
 		FangYu_ += 2;
-		HP_ += 20;
+		Hp_ += 20;
 	}
 	if (Level_ > Level)
 	{
@@ -380,7 +484,7 @@ void LevelUp(int GongJi_, int FangYu_, int HP_, int Level_, int Exp_)
 	Exp = Exp_;
 	GongJi = GongJi_;
 	FangYu = FangYu_;
-	HP = HP_;
+	Hp = Hp_;
 
 }
 void CheckName()
